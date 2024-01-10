b7c1ae3e300e415a591baf31876b6b18c4ba5804
Ravindu Weerakoon <w.m.t.ravindusampath@gmail.com>
2024-01-10 17:26:52 +0530

    add toggle button mechanism

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
 
+let currentStateOFTabel = false;
 
+function changeDayAndFull() {
+
+    if(currentStateOFTabel === false) {
+
+        Array.from(document.getElementsByClassName('day')).forEach(element => {
+            element.classList.toggle('hidden', ("d" + new Date().getDay()) !== element.id);
+    
+        });
+    
+        let element = document.getElementById("timetable");
+    
+        element.classList.add('dayTable');
+        element.classList.remove('fullTable');
+
+        currentStateOFTabel = true;
+
+    }  else{
+
+        Array.from(document.getElementsByClassName('day')).forEach(element => {
+            element.classList.remove('hidden', ("d" + new Date().getDay()) !== element.id);
+    
+        });
+    
+        let element = document.getElementById("timetable");
+    
+        element.classList.remove('dayTable');
+        element.classList.add('fullTable');
+
+        currentStateOFTabel = false;
+
+    }
+
+}
+
+
+document.getElementById("changeBtn").addEventListener("click", changeDayAndFull);
